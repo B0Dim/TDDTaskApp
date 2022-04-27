85fa75466a5309b62d41c67b6691f074ea91e9e3
B0Dim <bodim@me.com>
2022-04-27 20:23:55 +0300

    finished detailViewController for mapView and tests

--- TDDTaskAppTests/Controller/DetailViewControllerTests.swift
+++ TDDTaskAppTests/Controller/DetailViewControllerTests.swift
@@ -73,4 +73,15 @@ class DetailViewControllerTests: XCTestCase {
         setupTaskAndAppearanceTransition()
         XCTAssertEqual(sut.locationLabel.text, "Baz")
     }
+    
+    func testSettingTaskSetsDateLabel() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.dateLabel.text, "01.01.19")
+    }
+    
+    func testSettingTaskSetsMapView() {
+        setupTaskAndAppearanceTransition()
+        XCTAssertEqual(sut.mapView.centerCoordinate.latitude, 54.74801923, accuracy: 0.001)
+        XCTAssertEqual(sut.mapView.centerCoordinate.longitude, 56.01103876, accuracy: 0.001)
+    }
 }
